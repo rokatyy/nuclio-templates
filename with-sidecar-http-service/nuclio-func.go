fd658eb0e5e9dc59f7052f2b4af509245a2176ce
rokatyy <ekaterina_molchanova@mckinsey.com>
2024-01-29 12:05:26 +0000

    sidecar http service example

--- with-sidecar-http-service/nuclio-func.go
+++ with-sidecar-http-service/nuclio-func.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	nuclio "github.com/nuclio/nuclio-sdk-go"
 	"github.com/valyala/fasthttp"
 	"net/url"
+	"os"
 )
 
 func Handler(context *nuclio.Context, event nuclio.Event) (interface{}, error) {
 	context.Logger.Info("Got request, sending it to sidecar container")
 	req := fasthttp.AcquireRequest()
 	req.SetBody(event.GetBody())
-	sidecarHost, _ := url.JoinPath("http://0.0.0.0:9000", event.GetPath())
+	sidecarHost := os.Getenv("SIDECAR_HOST")
+	sidecarPort := os.Getenv("SIDECAR_PORT")
+	sidecarHost, _ := url.JoinPath(fmt.Sprintf("%s:%s", sidecarHost, sidecarPort), event.GetPath())
 	context.Logger.Info(sidecarHost)
 	req.SetRequestURI(sidecarHost)
 	resp := fasthttp.AcquireResponse()
